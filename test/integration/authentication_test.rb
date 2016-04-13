2974e789756c3dcd65750a3f3c975c5df50e4989
Josh Cheek <josh.cheek@gmail.com>
2016-04-13 15:08:36 -0500

    Refactor auth mocking into its own method

--- test/integration/authentication_test.rb
+++ test/integration/authentication_test.rb
@@ -1,17 +1,19 @@
 require 'test_helper'
 
 class AuthenticationTest < ActionDispatch::IntegrationTest
-  def test_users_can_sign_in_with_twitter_for_the_first_time
-    name = 'Josh'
-    uid  = '123test-uid456'
-
-    # mock out authentication
+  def mock_mah_auth(name, uid)
     OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
       'provider'    => 'twitter',
       'uid'         => uid,
       'info'        => {'name' => name},
       'credentials' => {'token' => 'test-token', 'secret' => 'test-secret'}
     })
+  end
+
+  def test_users_can_sign_in_with_twitter_for_the_first_time
+    name = 'Josh'
+    uid  = '123test-uid456'
+    mock_mah_auth name, uid
 
     page.visit root_path
 
@@ -38,14 +40,7 @@ class AuthenticationTest < ActionDispatch::IntegrationTest
   def test_users_can_sign_in_with_twitter_when_returning_to_the_site
     name = 'Josh'
     uid  = '123test-uid456'
-
-    # mock out authentication
-    OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
-      'provider'    => 'twitter',
-      'uid'         => uid,
-      'info'        => {'name' => name},
-      'credentials' => {'token' => 'test-token', 'secret' => 'test-secret'}
-    })
+    mock_mah_auth name, uid
 
     user = User.create! name: name, uid: uid
     page.visit root_path
